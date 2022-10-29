129c5b611ec48cc9da6506781c961c71c06e9aa5
DangKien98 <dangvankien98@live.com>
2022-10-30 02:41:16 +0700

    Xoá cái Warning

--- Journal-be/Models/JournalContext.cs
+++ Journal-be/Models/JournalContext.cs
@@ -32,7 +32,6 @@ namespace Journal_be.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseMySql("server=oggycute.tplinkdns.com;port=31100;user=root;password=password;database=Journal", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
             }
         }
